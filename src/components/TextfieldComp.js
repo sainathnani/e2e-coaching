6dad1c5150f99639095aab9542e73abba926b4b5
saiintellect <venkata.sanagapalli@intellectdesign.com>
2018-09-06 12:14:01 +0530

    Added Textfield component

---
+++ src/components/TextfieldComp.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+const TextfieldComp = function (props) {
+    return(
+        <label>{props.name}:
+            <input type="textfield" />
+        </label>
+    )
+}
+
+export default TextfieldComp;
